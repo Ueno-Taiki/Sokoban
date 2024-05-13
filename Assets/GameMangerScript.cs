b475aec7c0279d350e83ccabe955437846296bf7
上野太己 <k023g0051@g.neec.ac.jp>
2024-05-13 11:19:42 +0900

    玉を移動

--- Assets/GameMangerScript.cs
+++ Assets/GameMangerScript.cs
@@ -1,66 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class GameMangerScript : MonoBehaviour
 {
+    public GameObject playerPrefab;
+
     //配列の作成
-    int[] map;
+    int[,] map;
+
+    //ゲーム管理用の配列
+    GameObject[,] field;
 
     // Start is called before the first frame update
     void Start()
     {
-        //配列の実態の作成と初期化
-        map = new int[] { 0, 0, 0, 1, 0, 2, 0, 0, 0 };
-        PrintArray();
+        //mapの生成
+        map = new int[,]{
+            { 1, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 }
+        };
+        field = new GameObject
+            [
+            map.GetLength(0),
+            map.GetLength(1)
+            ];
+
+        string debugText = "";
+
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[y, x] == 1)
+                {
+                    field[y, x] = Instantiate(
+                        playerPrefab,
+                        new Vector3(x, map.GetLength(0) - 1 - y, 0), Quaternion.identity);
+                }
+                debugText += map[y, x].ToString() + ",";
+            }
+            debugText += "\n";
+        }
+        Debug.Log(debugText);
+        //PrintArray();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            //メソット化した処理を使用
-            int playerIndex = GetPlayerIndex();
-
-            //移動処理を関数化
-            MoveNumber(1, playerIndex, playerIndex + 1);
-            PrintArray();
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(0, -1));
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(0, 1));
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(-1, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(1, 0));
         }
     }
 
-    void PrintArray()
-    {
-        //文字列の宣言と初期化
-        string debugText = "";
-
-        for(int i = 0; i < map.Length; i++)
-        {
-            //文字列を連携していく
-            debugText += map[i].ToString() + ",";
-        }
-
-        //結合した文字列を出力
-        Debug.Log(debugText);
-    }
-
-    int GetPlayerIndex()
+    Vector2Int GetPlayerIndex()
     {
         //要素数はmap.Lengthで取得
-        for (int i = 0; i < map.Length; i++)
+        for (int y = 0; y < map.GetLength(0); y++)
         {
-            if (map[i] == 1)
+            for (int x = 0; x < map.GetLength(1); x++)
             {
-                return i;
+                //nullだったらタグを調べず次の要素へ移る
+                if (field[y, x] == null) { continue; }
+                //タグの確認を行う
+                if (field[y, x].tag == "Player")
+                {
+                    return new Vector2Int(x, y);
+                }
             }
         }
-        return -1;
+        return new Vector2Int(-1, -1);
     }
 
-    bool MoveNumber(int number, int moveFrom, int moveTo)
+    bool MoveNumber(Vector2Int moveFrom, Vector2Int moveTo)
     {
-        //移動先が範囲外なら移動不可
-        if (moveTo < 0 || moveTo >= map.Length) { return false; }
+        //二次元配列に対応
+        if (moveTo.y < 0 || moveTo.y >= field.GetLength(0)) { return false; }
+        if (moveTo.x < 0 || moveTo.x >= field.GetLength(1)) { return false; }
+        /*
         //移動先に2が居たら
         if (map[moveTo] == 2)
         {
@@ -70,9 +107,12 @@ public class GameMangerScript : MonoBehaviour
             //もし箱が移動失敗したら、プレイヤーの移動も失敗
             if (!success) { return false; }
         }
+        */
         //プレイヤー・箱関わらずの移動処理
-        map[moveTo] = number;
-        map[moveFrom] = 0;
+        field[moveTo.y, moveTo.x] = field[moveFrom.y, moveFrom.x];
+        field[moveFrom.y, moveFrom.x].transform.position =
+            new Vector3(moveTo.x, map.GetLength(0) - moveTo.y, 0);
+        field[moveFrom.y, moveFrom.x] = null;
         return true;
     }
 }
